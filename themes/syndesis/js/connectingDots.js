21aea7a72cc1053e3415e47a5b5b1990fab2ce08
Zoran Regvart <zregvart@apache.org>
2017-05-11 13:06:07 +0200

    style(home): New background animation
    I propose a new background animation that resembles the current logo
    icon, it is based on the work by Elton Kamami
    (https://codepen.io/eltonkamami):
    https://codepen.io/eltonkamami/pen/ECrKd licensed under MIT license.

--- themes/syndesis/js/connectingDots.js
+++ themes/syndesis/js/connectingDots.js
@@ -1,154 +1,97 @@
 "use strict";
 
-// based on http://codepen.io/erikterwan/pen/VamXGe by http://codepen.io/erikterwan/
-// MIT licensed
-var connectingDots = (function(window) {
-    return function (documentId, o) {
-        o = o || {}
-        var canvas = document.getElementById(documentId),
-          ctx = canvas.getContext('2d'),
-          points = [],
-          w, h,
-          delay = o.delay || 100,
-          amount = o.amount || 100, // relative to screen size
-          speed = o.speed || 30,
-          size = o.size || 4,
-          lineWidth = o.lineWidth || 2,
-          distance = o.distance || 500,
-          randomSize = o.randomSize || 0.5,
-          bgColor = o.bgColor || '#0088CE';
+// based on https://codepen.io/eltonkamami/pen/ECrKd by Elton Kamami (https://codepen.io/eltonkamami)
+// MIT license
+(function() { // block start
+    var canvas = document.getElementById('particles'),
+        ctx = canvas.getContext('2d'),
+        particles = [],
+        patriclesNum = 50,
+        w = window.innerWidth,
+        h = 305,
+        colors = ['#a30000', '#eee', '#51e8ff'];
 
-        function setCanvasSize() {
-            w = canvas.width = canvas.parentNode.offsetWidth;
-            h = canvas.height = canvas.parentNode.offsetHeight;
-        }
+    canvas.width = w;
+    canvas.height = h;
 
-        function setup() {
-            setCanvasSize();
+    function Dot() {
+        this.x = Math.round(Math.random() * w);
+        this.y = Math.round(Math.random() * h);
+        this.rad = Math.round(Math.random() * 1) + 1;
+        this.irgba = colors[Math.round(Math.random() * 3)];
+        this.orgba = colors[Math.round(Math.random() * 3)];
+        this.vx = Math.round(Math.random())/8 - 1/16;
+        this.vy = Math.round(Math.random())/8 - 1/16;
+    }
 
-            points = [];
-            var screenDelta = Math.sqrt(w + h) / 100;
-            var useAmount = amount * screenDelta;
+    function draw() {
+        ctx.clearRect(0, 0, w, h);
+        ctx.globalCompositeOperation = 'lighter';
+        for (var i = 0; i < patriclesNum; i++) {
+            var temp = particles[i];
+            var factor = 1;
 
-            for (var i = 0; i < useAmount; i++) {
-                var y = (Math.random() * h);
-                var ySpeed = (Math.random() * (speed / 10)) - (speed / 20);
+            for (var j = 0; j < patriclesNum; j++) {
 
-                var x3rd = (Math.random() * (w/3));
+                var temp2 = particles[j];
+                ctx.linewidth = 0.5;
 
-                var x = (i % 2) == 0 ? x3rd : w - x3rd;
-                var xSpeed = (Math.random() * (speed / 10)) - (speed / 20);
-
-                points.push(new Point(x, y, xSpeed, ySpeed));
+                if (temp.rgba == temp2.rgba && findDistance(temp, temp2) < 100) {
+                    ctx.strokeStyle = temp.orgba;
+                    ctx.beginPath();
+                    ctx.moveTo(temp.x, temp.y);
+                    ctx.lineTo(temp2.x, temp2.y);
+                    ctx.stroke();
+                    factor++;
+                }
             }
 
-            ctx.fillStyle = bgColor;
-            ctx.fillRect(0, 0, w, h);
+            ctx.fillStyle = temp.irgba;
+            ctx.strokeStyle = temp.orgba;
+
+            ctx.beginPath();
+            ctx.arc(temp.x, temp.y, temp.rad * factor, 0, Math.PI * 2, true);
+            ctx.fill();
+            ctx.closePath();
+
+            ctx.beginPath();
+            ctx.arc(temp.x, temp.y, (temp.rad + 5) * factor, 0, Math.PI * 2, true);
+            ctx.stroke();
+            ctx.closePath();
+
+            temp.x += temp.vx;
+            temp.y += temp.vy;
+
+            if (temp.x > w) temp.x = 0;
+            if (temp.x < 0) temp.x = w;
+            if (temp.y > h) temp.y = 0;
+            if (temp.y < 0) temp.y = h;
         }
-
-        function draw() {
-            ctx.globalCompositeOperation = "source-over";
-
-            ctx.fillStyle = bgColor;
-            ctx.fillRect(0, 0, w, h);
-
-            ctx.lineWidth = lineWidth;
-            ctx.fillStyle = 'hsl(190,55%,54%)';
-
-            var screenDelta = Math.sqrt(w + h) / 100;
-            var useDistance = distance * screenDelta;
-
-            ctx.globalCompositeOperation = "lighter";
-
-            points.each(function (point) {
-                points.each(function (connection) {
-                    var distanceX = Math.pow((connection.x - point.x), 2);
-                    var distanceY = Math.pow((connection.y - point.y), 2);
-                    var distance = Math.sqrt(distanceX + distanceY);
-
-                    if (distance <= useDistance) {
-                        var alpha = 1.0 - (distance / useDistance);
-
-                        ctx.strokeStyle = 'hsla(190,65%,10%, ' + alpha + ')';
-
-                        ctx.beginPath();
-                        ctx.moveTo(point.x, point.y);
-                        ctx.lineTo(connection.x, connection.y);
-                        ctx.stroke();
-                        ctx.closePath();
-                    }
-                });
-            });
-
-            ctx.globalCompositeOperation = "source-over";
-
-            points.each(function (point) {
-                point.draw();
-            });
-
-            setTimeout(function () {
-                window.requestAnimationFrame(draw);
-            }, delay)
-        }
-
-        var Point = function (_x, _y, _xSpeed, _ySpeed) {
-            this.x = _x;
-            this.y = _y;
-            this.xSpeed = _xSpeed;
-            this.ySpeed = _ySpeed;
-
-            var _this = this;
-
-            this.draw = function () {
-                var xNoise = ((Math.random() * randomSize) - randomSize / 2);
-                var yNoise = ((Math.random() * randomSize) - randomSize / 2);
-
-                _this.x += _this.xSpeed + xNoise;
-                _this.y += _this.ySpeed + yNoise;
-
-                if (_this.x < size || _this.x > (w - size)) {
-                    _this.xSpeed = -_this.xSpeed;
-                }
-
-                if (_this.y < size || _this.y > (h - size)) {
-                    _this.ySpeed = -_this.ySpeed;
-                }
-
-                if (_this.x < 0) {
-                    _this.x = 2;
-                }
-
-                if (_this.x > w) {
-                    _this.x = w - 2;
-                }
-
-                if (_this.y < 0) {
-                    _this.y = 2;
-                }
-
-                if (_this.y > h) {
-                    _this.y = h - 2;
-                }
-
-                ctx.beginPath();
-                ctx.arc(_this.x, _this.y, size, 0, 2 * Math.PI);
-                ctx.fill();
-                ctx.closePath();
-            };
-        }
-
-        // Faster than .forEach
-        Array.prototype.each = function (a) {
-            var l = this.length;
-            for (var i = 0; i < l; i++)a(this[i], i)
-        };
-
-        window.addEventListener('resize', setup, false);
-
-        setTimeout(function() {
-            setCanvasSize()
-            setup();
-            draw();
-        }, 100);
     }
-})(window);
+
+    function findDistance(p1, p2) {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
+
+    window.requestAnimFrame = (function() {
+        return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            function(callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+
+    (function init() {
+        for (var i = 0; i < patriclesNum; i++) {
+            particles.push(new Dot);
+        }
+    })();
+
+    (function loop() {
+        draw();
+        requestAnimFrame(loop);
+    })();
+
+})(); // end block
+
